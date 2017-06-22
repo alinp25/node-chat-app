6eb16d2d5003e55520d42a8fdd9bb243a9edb27f
Alin Pisica <alinp2508@gmail.com>
2017-06-22 12:52:35 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit("createMessage", {
-        from: 'jen@example.com',
-        text: 'Hey, this is Jen!'
-    });
 });
 
 socket.on('disconnect', function () {
