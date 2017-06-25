24f498ac4758bc3bf52b9ed31ec5ff712b86efad
Alin Pisica <alinp2508@gmail.com>
2017-06-24 23:42:33 -0500

    Add mustache.js for message templates

--- playground/time.js
+++ playground/time.js
@@ -7,9 +7,5 @@ const moment = require('moment');
 // date.add(1, 'y').subtract(9, 'M');
 // console.log(date.format('MMM Do, YYYY'));
 
-var someTimeStamp = moment().valueOf();
-console.log(someTimeStamp);
-
-var createdAt = 1234;
-var date = moment(createdAt);
-console.log(date.format('HH:mm a'));
+var date = moment();
+console.log(date.format('HH:mm ac   '));
